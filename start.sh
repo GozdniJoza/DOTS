f27dc7fbf68671a2b953cca38be9c7302cf335b4
GozdniJoza <98474002+GozdniJoza@users.noreply.github.com>
2022-02-20 19:50:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
+#./bin/bash
 sudo pacman -Syu
-sudo pacman -S --needed git base-devel discord rxvt-unicode firefox thunar ttf-roboto
+sudo pacman -S --needed git base-devel discord rxvt-unicode firefox thunar ttf-roboto ttf-font-awesome pulseaudio alsa-utils vim kate compton
 
 
 git clone https://aur.archlinux.org/yay.git
