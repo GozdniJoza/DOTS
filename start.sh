30f7205195da352520f9420ab87beee5905314b3
GozdniJoza <98474002+GozdniJoza@users.noreply.github.com>
2022-02-20 12:44:37 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,13 @@
+sudo pacman -Syu
+sudo pacman -S --needed git base-devel discord rxvt-unicode firefox thunar ttf-roboto
+
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay visual-studio-code-bin
+
+
+
+
